9361d3f5837f3337d2526f533d7a01d061635cec
Matias Peterka <matias.tauro@hotmail.com>
2025-06-29 10:53:00 -0300

    Agrego CerrarSesion

---
+++ TPC_PROG_III/Usuario/CerrarSesion.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Title="Cerrar Sesión" Language="C#" MasterPageFile="~/Compartido/Site.Master" AutoEventWireup="true" CodeBehind="CerrarSesion.aspx.cs" Inherits="TPC_PROG_III.CerrarSesion" %>
+
---
+++ TPC_PROG_III/Usuario/CerrarSesion.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_PROG_III
+{
+    public partial class CerrarSesion : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+            Response.Redirect("/cliente/Default.aspx");
+        }
+    }
+}
